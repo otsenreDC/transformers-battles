308085cf42430276d4ad71032c086fbfbfafaa6c
Ernesto De los Santos <ernestodlsc@gmail.com>
2021-04-06 13:26:56 -0400

    :fire: Remove the Provider Installer and Google Services dependency

--- app/src/main/java/io/banana/transformersbattle/framework/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/banana/transformersbattle/framework/ui/splash/SplashActivity.kt
@@ -2,11 +2,8 @@ package io.banana.transformersbattle.framework.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.GoogleApiAvailability
-import com.google.android.gms.security.ProviderInstaller
 import io.banana.transformersbattle.R
 import io.banana.transformersbattle.databinding.ActivitySplashBinding
 import io.banana.transformersbattle.framework.extensions.observe
@@ -15,7 +12,7 @@ import org.koin.android.ext.android.inject
 
 private const val ERROR_DIALOG_REQUEST_CODE = 1
 
-class SplashActivity : AppCompatActivity(), ProviderInstaller.ProviderInstallListener {
+class SplashActivity : AppCompatActivity() {
 
     private var retryProviderInstall: Boolean = false
 
@@ -34,8 +31,11 @@ class SplashActivity : AppCompatActivity(), ProviderInstaller.ProviderInstallLis
 
         registerEvents()
 
-        ProviderInstaller.installIfNeededAsync(this, this)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        mainViewModel.start()
     }
 
     private fun registerEvents() {
@@ -45,65 +45,5 @@ class SplashActivity : AppCompatActivity(), ProviderInstaller.ProviderInstallLis
                 finish()
             }
         }
-    }
-
-    /**
-     * This method is only called if the provider is successfully updated
-     * (or is already up-to-date).
-     */
-    override fun onProviderInstalled() {
-        // Provider is up-to-date, app can make secure network calls.
-        mainViewModel.start()
-    }
-
-    override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) {
-        GoogleApiAvailability.getInstance().apply {
-            if (isUserResolvableError(errorCode)) {
-                // Recoverable error. Show a dialog prompting the user to
-                // install/update/enable Google Play services.
-                showErrorDialogFragment(this@SplashActivity, errorCode, ERROR_DIALOG_REQUEST_CODE) {
-                    // The user chose not to take the recovery action
-                    onProviderInstallerNotAvailable()
-                }
-            } else {
-                onProviderInstallerNotAvailable()
-            }
-        }
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ERROR_DIALOG_REQUEST_CODE) {
-            // Adding a fragment via GoogleApiAvailability.showErrorDialogFragment
-            // before the instance state is restored throws an error. So instead,
-            // set a flag here, which will cause the fragment to delay until
-            // onPostResume.
-            retryProviderInstall = true
-        }
-    }
-
-    /**
-     * On resume, check to see if we flagged that we need to reinstall the
-     * provider.
-     */
-    override fun onPostResume() {
-        super.onPostResume()
-        if (retryProviderInstall) {
-            // We can now safely retry installation.
-            ProviderInstaller.installIfNeededAsync(this, this)
-        }
-        retryProviderInstall = false
-    }
-
-    private fun onProviderInstallerNotAvailable() {
-        // This is reached if the provider cannot be updated for some reason.
-        // App should consider all HTTP communication to be vulnerable, and take
-        // appropriate action.
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
-
     }
 }
--- readme.md
+++ readme.md
@@ -1,15 +1,15 @@
 # transformers-battles
 Demo app
 
-This app performs battles betweewn Autobots and Decepticons.
+This app performs battles between Autobots and Decepticons.
 
 
 ### Getting started
 
 1. Download the repository
 2. Open Android Studio
-3. Include a google-serivces.json file , in the ./app directory, granting access to this project id io.banana.transformersbattle
-4. Run the emulador
+3. Include a google-services.json file , in the ./app directory, granting access to this project id io.banana.transformersbattle
+4. Run the emulator
 
 ### Battles assumptions
 
@@ -26,7 +26,7 @@ This app performs battles betweewn Autobots and Decepticons.
 1. Lack of support por Android 4.4 and below 
   Due to several changes on the network security in the Transport Layer, this app doesn't complete the handshake with the server. 
   An implementation was provided using a solution from the Android Developers site, but it hasn't been
-  completly tested on a physical device, the problem is still present in the Emulator.
+  completely tested on a physical device, the problem is still present in the Emulator.
   Solution implemented: https://developer.android.com/training/articles/security-gms-provider
   TLS test ran with: https://www.cdn77.com/tls-test
 2. Sometimes Glide fails to fetch the team icon
